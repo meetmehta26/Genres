2bb834fef01c9817cbba4a7adbb9d5e65dec90a4
MEHTA Meet <meet7b@gmail.com>
2020-09-10 00:15:20 +0530

    Added mongo connection and saved data to it

--- router/generes.js
+++ router/generes.js
@@ -1,5 +1,31 @@
 const router = require('express').Router();
 const Joi = require('@hapi/joi')
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/Vidly')
+    .then(() => console.log('Successfully connected to the database'))
+    .catch(() => console.log('Some error occured while connecting to the database'))
+
+const schema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+})
+
+async function createData(name1) {
+
+    const Genere = mongoose.model('generes', schema);
+    const genere = Genere({
+        name: name1
+    })
+    const result=await genere.save();
+    console.log(result);
+    return result;
+}
+
+
+
 
 var generes = [
     { id: 1, name: "generes1" },
@@ -12,15 +38,17 @@ router.get('/', (req, res) => {
     res.status(200).send(generes);
 })
 
-router.post('/',  (req, res) => {
-    const {error} =  validateGenre(req.body);
+router.post('/', async(req, res) => {
+    const { error } = validateGenre(req.body);
     if (error) { return res.status(400).send(error.details[0].message) }
-    const genere = {
-        id: generes.length + 1,
-        name: req.body.name
-    }
-    generes.push(genere);
-    res.status(200).send(genere);
+    // const genere = {
+    //     id: generes.length + 1,
+    //     name: req.body.name
+    // }
+    const data=await createData(req.body.name)
+    // generes.push(genere);
+    console.log("DATA",data);
+    res.status(200).send("Successfully added the data");
 })
 
 router.put('/:id', (req, res) => {
@@ -31,11 +59,11 @@ router.put('/:id', (req, res) => {
     genere.name = req.body.name;
     res.status(200).send(genere);
 })
-router.delete('/:id',(req,res)=>{
+router.delete('/:id', (req, res) => {
     const genere = generes.find(s => s.id === parseInt(req.params.id));
     if (!genere) { return res.status(404).send('Resource Not found'); }
     const index = generes.indexOf(genere);
-    generes.splice(index,1);
+    generes.splice(index, 1);
     res.send(genere);
 })
 router.get('/:id', (req, res) => {
@@ -48,8 +76,8 @@ function validateGenre(genre) {
     const schema = {
         name: Joi.string().min(5).required()
     }
-    console.log(genre,"typeof",typeof(genre));
-    return Joi.validate(genre,schema );
+    console.log(genre, "typeof", typeof (genre));
+    return Joi.validate(genre, schema);
 
 }
 

733cf34c83229e71c4d8f3be3ee614cdde677250
MEHTA Meet <meet7b@gmail.com>
2020-09-12 19:26:31 +0530

    Added Mongconnection and

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const Joi = require('@hapi/joi')
 const generes = require('./router/generes');
+const mongoose = require('mongoose');
 const app = express();
 
 
 
+
+mongoose.connect('mongodb://localhost/Vidly')
+    .then(() => console.log('Successfully connected to the database'))
+    .catch(() => console.log('Some error occured while connecting to the database'))
 
 
 app.use(express.json());

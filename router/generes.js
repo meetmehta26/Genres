e60673603508b08c9db1434f466546392394a248
MEHTA Meet <meet7b@gmail.com>
2020-09-12 19:27:11 +0530

    Get,Put,Post request to manipulate data from db

--- router/generes.js
+++ router/generes.js
@@ -2,62 +2,68 @@ const router = require('express').Router();
 const Joi = require('@hapi/joi')
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/Vidly')
-    .then(() => console.log('Successfully connected to the database'))
-    .catch(() => console.log('Some error occured while connecting to the database'))
+
 
 const schema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: true,
+        min: 4,
+        max: 50
     },
 })
-
+const Genere = mongoose.model('generes', schema);
 async function createData(name1) {
 
-    const Genere = mongoose.model('generes', schema);
+
     const genere = Genere({
         name: name1
     })
-    const result=await genere.save();
+    const result = await genere.save();
     console.log(result);
     return result;
 }
+// async function findbyID(){
+//     const Genere = mongoose.model('generes', schema);
+//     const genere = Genere({
+//         name: name1
+//     })
+//     const result=await genere.save();
+//     console.log(result);
+//     return result;
+
+// }
 
 
 
 
-var generes = [
-    { id: 1, name: "generes1" },
-    { id: 2, name: "generes2" },
-    { id: 3, name: "generes3" },
-    { id: 4, name: "generes4" },
-]
+// var generes = [
+//     { id: 1, name: "generes1" },
+//     { id: 2, name: "generes2" },
+//     { id: 3, name: "generes3" },
+//     { id: 4, name: "generes4" },
+// ]
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+    const generes = await Genere.find();
     res.status(200).send(generes);
 })
 
-router.post('/', async(req, res) => {
+router.post('/', async (req, res) => {
     const { error } = validateGenre(req.body);
     if (error) { return res.status(400).send(error.details[0].message) }
-    // const genere = {
-    //     id: generes.length + 1,
-    //     name: req.body.name
-    // }
-    const data=await createData(req.body.name)
-    // generes.push(genere);
-    console.log("DATA",data);
+    const data = await createData(req.body.name)
+    console.log("DATA", data);
     res.status(200).send("Successfully added the data");
 })
 
-router.put('/:id', (req, res) => {
-    const genere = generes.find(s => s.id === parseInt(req.params.id));
-    if (!genere) { return res.status(404).send('Resource Not found'); }
+router.put('/:id', async (req, res) => {
     const { error } = validateGenre(req.body);
     if (error) { return res.status(400).send(error.details[0].message) }
-    genere.name = req.body.name;
-    res.status(200).send(genere);
+    const genere = await Genere.findByIdAndUpdate(req.params.id, { name: req.body.name }, { new: true })
+    if (!genere) { return res.status(404).send('Resource Not found'); }
+    const result = await genere.save();
+    res.status(200).send(result);
 })
 router.delete('/:id', (req, res) => {
     const genere = generes.find(s => s.id === parseInt(req.params.id));
